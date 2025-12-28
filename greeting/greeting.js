34771e30876bc44b3d16b28ce9ba5bb79d8a8377
anurag <yourboyanurag777@gmail.com>
2025-12-28 09:26:18 +0530

    Added a file for greeeting according to  current time

---
+++ greeting/greeting.js
@@ -0,0 +1,19 @@
+import os from 'node:os'
+console.log(os.cpus())
+
+
+const name   = "Anurag";
+const hour  =  Date.now.toString();
+console.log(hour)
+console.log(name)
+
+
+
+function greeting(hour){
+    if(hour<4|| hour>=19) return "Good night"
+    if(hour>9)return "Good Morning"
+    if(hour<16) return "Good Afternoon"
+    return "Good evening"
+}
+ const greetings  = greeting(hour);
+ console.log(`${greeting} ${name}`);
