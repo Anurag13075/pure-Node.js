97db3b18176b63d6d5c9190a73a9f560d1139118
anurag <yourboyanurag777@gmail.com>
2025-12-28 11:41:00 +0530

    First http server

---
+++ First-http-server.js/http.js
@@ -0,0 +1,23 @@
+//Creating of first http server in node.js .
+
+
+import http from 'http'; // http is a built-in module in Node.js to create web servers
+
+
+// Create an HTTP server by createServer method 
+
+// req => req is a method which is used to get request from client.
+// res => res is a method which is used to send response to client .
+
+
+const server   = http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end('Hello World\n');
+})
+
+
+const port  = 3000;
+//now server is listening on port 3000 go to your browser and go to localhost 3000 to see things in action .
+
+// listen => listen is a method to listen on specific port .
+server.listen(port, `server is listening on port${port}` );
