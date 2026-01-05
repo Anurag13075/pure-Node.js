9d8fd41d6882e1071024ab4428f1083debce519e
anurag <yourboyanurag777@gmail.com>
2026-01-05 11:24:32 +0530

    server

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,9 @@ const server = http.createServer((req, res) => { // creation of an HTTP server
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello, this is the routes.js file!\n');
 });
+
+
+
 const PORT  = 3000;
 server.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
