30865cf6b15a5b06957c341b90b8e5a733f616e9
anurag <yourboyanurag777@gmail.com>
2026-01-13 08:16:00 +0530

    A method on gile to read and write data through stream

--- stream.js
+++ stream.js
@@ -4,7 +4,32 @@
 //stream module is very useful for handling large files or data .
 import {Readble } from 'stream';
 import { Writable } from 'stream';
+import fs, { read } from  'fs';
 
 
 
 //creating a readble stream
+const raddblestream = new Readble({
+    const file =  fs.createReadStream(addEventListener.txt);
+    read(size){
+        const chunk = file.read(size);
+        if(chunk){
+            this.push(chunk);
+            console.log(file);
+
+        } else {
+            this.push(null);
+
+        }
+    }
+   
+    
+})
+readblestream.on('data',(chunk)=>{
+    console.log(`received ${chunk.length} bytes of data`);
+});
+
+readblestream.on('end',()=>{
+    console.log('no more data');
+});
+readblestream(file);
