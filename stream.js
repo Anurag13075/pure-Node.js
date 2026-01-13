36ff6d0d335187a3d8855049d710e80f890f1d52
anurag <yourboyanurag777@gmail.com>
2026-01-13 08:10:05 +0530

    Readable and writable stream

---
+++ stream.js
@@ -0,0 +1,10 @@
+// It is about the stream module in node js .
+//stream module is used to handle streaming data in node js .
+//stream module is used to read or write data piece by piece instead of all at once .
+//stream module is very useful for handling large files or data .
+import {Readble } from 'stream';
+import { Writable } from 'stream';
+
+
+
+//creating a readble stream
