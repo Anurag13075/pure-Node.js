988faa6685d3ccb6b8e36f8ac1c4cc67e8917c1a
anurag <yourboyanurag777@gmail.com>
2026-01-28 15:32:15 +0530

    routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,15 @@
 import http from 'http';
 
 
+
 const server = http.createServer((req, res) => { // creation of an HTTP server
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello, this is the routes.js file!\n');
 });
+
+http.get("about", (res)=>{
+    res.end("hey about")
+})
 console.log("Server is starting...");
 
 
