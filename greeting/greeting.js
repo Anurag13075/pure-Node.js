c0796e6a57d4dd2d9982d0e2841bbd1edfb1d836
anurag <yourboyanurag777@gmail.com>
2026-01-28 15:30:53 +0530

    greet

--- greeting/greeting.js
+++ greeting/greeting.js
@@ -5,17 +5,7 @@ console.log(os.cpus())
 const name   = "Anurag";
 const hour  =  Date.now.toString();
 console.log(hour)
-console.log(name)
-
-
-
-
-
-
-
-
-
-
+console.log(name);
 
 function greeting(hour){
     if(hour<4|| hour>=19) return "Good night"
@@ -24,4 +14,4 @@ function greeting(hour){
     return "Good evening"
 }
  const greetings  = greeting(hour);
- console.log(`${greeting} ${name}`);
+ 
