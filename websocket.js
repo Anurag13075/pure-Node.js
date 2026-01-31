210d713be9d58bb6d8073ce9e9c239c350f1926a
anurag <yourboyanurag777@gmail.com>
2026-01-31 10:59:49 +0530

    realtime-communication with websocketes

--- websocket.js
+++ websocket.js
@@ -1,1 +1,11 @@
-import 
+const socket  =  new WebSocket('ws://localhost:8080');
+
+
+socket.addEventListener('open', function(event){
+    console.log('WebSocket is open now.');
+    socket.send('hwllo server')
+    
+})
+socket.addEventListener('message', function(event){
+    console.log('Message from server ', event.data);
+})
